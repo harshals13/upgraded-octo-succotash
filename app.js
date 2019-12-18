060195882fb1db3e85ebe0a19606b6c0a0918523
Harsha <harsha@minebrat.com>
2019-12-19 01:08:37 +0530

    c

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var config = require('./config/config');
 /*
  * Start node server
  */
-app.listen(config.port);
+app.listen(config.port, '0.0.0.0');
 app.timeout = 1000;
 console.log("Server started on " + config.port);
