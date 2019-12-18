d705b99329fff8a9a476fabafcfd0346ef07466f
Harsha <harsha@minebrat.com>
2019-12-18 23:35:13 +0530

    remote

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/myAppTwo', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://dbUser:WcqmtZqYTwpwFlZV@cluster0-rmn8p.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB connection Succeeded') }
     else { console.log('Error in DB connection : ' + err)}
 });
