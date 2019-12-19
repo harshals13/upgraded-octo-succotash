9a35c510a443294e1474d706023934dd8780271a
Harsha <harsha@minebrat.com>
2019-12-19 10:29:40 +0530

    path to public

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
 // Connecting to database
 require('./models/db')
 require("./models/location.model");
